a00681b41d38c37761d48cf9bb5a7a4a50af11e2
parvbilla <parvbilla2004@gmail.com>
2026-08-04 12:29:16 +0530

    Day 3: Added Admin  page

---
+++ app/admin/about/layout.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Layout() {
+    return (
+        <>
+            <html>
+                <body>
+                    <div>
+                            <Link href="/">Back Home</Link>
+                    </div>
+                    <h1>Admin About Layout...!</h1>
+                </body>
+            </html>
+
+        </>
+    )
+}
